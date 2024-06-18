464110e832c74aba73aa08d62faa4132c5c181d4
raie03 <raie.iwatsuki@gmail.com>
2024-06-19 05:37:02 +0900

    youtube

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -6,16 +6,18 @@ import YouTube from 'react-youtube';
 
 
 const Detail = () => {
-    const params = useParams();
-    const foundArticle = data[params.id-1];
+  const params = useParams();
+  const foundArticle = data[params.id - 1];
   return (
-    <div>
+    <div className="container text-center">
+      <div className='detailPage'>
         <h2 className='detailTitle'>{foundArticle.title}</h2>
         <p className='detailContent'>{foundArticle.content}</p>
         <div class="you">
-        <iframe class="youtube_ifreme" width="560" height="315" src={foundArticle.youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe class="youtube_ifreme" width="560" height="315" src={foundArticle.youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
         </div>
+      </div>
     </div>
   )
 }
