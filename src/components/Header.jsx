a3b1d39ea1eb97ba5a7ba15112f8b2f5c5234345
raie03 <raie.iwatsuki@gmail.com>
2024-06-18 13:51:37 +0900

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
     return <header>
         <div className="logo">
-            <h3>amagoi06'sPORTFOLIO</h3>
+            <h3>PORTFOLIO</h3>
         </div>
 
         <nav>
