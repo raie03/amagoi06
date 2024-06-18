234466903670bc154b3843c8aaaad18ad2576efe
raie03 <raie.iwatsuki@gmail.com>
2024-06-19 07:01:15 +0900

    bio

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -10,6 +10,8 @@ const Profile = () => {
             <div class="bio">
                 <div>DTM歴4年。世界観のある曲が好きです。</div>
                 <div>Twitter: <a href="https://x.com/Am45106">https://x.com/Am45106</a></div>
+                <div>YouTube: <a href="https://www.youtube.com/@amagoi06">https://www.youtube.com/@amagoi06</a></div>
+                <div>SoundCloud: <a href="https://soundcloud.com/am451">https://soundcloud.com/am451</a></div>
                 <div>contact: amagoi06.work@gmail.com</div>
             </div>
         </div>
