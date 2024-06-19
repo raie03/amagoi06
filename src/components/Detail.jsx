cd443298605819cdabc19d32fea7bfad2c329648
raie03 <raie.iwatsuki@gmail.com>
2024-06-20 05:47:59 +0900

    json

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -14,8 +14,10 @@ const Detail = () => {
         <h2 className='detailTitle'>{foundArticle.title}</h2>
         <p className='detailContent'>{foundArticle.content}</p>
         <div class="you">
-          <iframe class="youtube_ifreme" width="560" height="315" src={foundArticle.youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
+          {foundArticle.youtube && <iframe class="youtube_ifreme" width="560" height="315" src={foundArticle.youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>}
+          {foundArticle.soundcloud && <iframe class="soundcloud" width="100%" height="315" src={foundArticle.soundcloud +"&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"}></iframe>}
+          
+          
         </div>
       </div>
     </div>
